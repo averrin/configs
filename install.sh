fa9e5d9f7ae601241a93f03903067fc93cf76981
Averrin <averrin@gmail.com>
2013-09-05 08:43:09 +0000

    add install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
-sudo apt-get install tmux zsh
+sudo apt-get install tmux zsh git-core
+get clone https://github.com/averrin/maximum-awesome.git
+cd ./maximum-awesome
 cp -r ./tmux.conf ~/.tmux.conf
 cp ./zshrc ~/.zshrc
 cp ./vimrc ~/.vimrc
 cp -r vim ~/.vim
 cp -r ~/.vim/vim/* ~/.vim
 cp -r ./oh-my-zsh ~/.oh-my-zsh
+tmux -u
