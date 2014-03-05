b2e286fceb3506dec724b55e17a9d16e748eaf05
Averrin <averrin@gmail.com>
2014-03-05 12:10:28 +0300

    add dircolors

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-sudo apt-get install tmux zsh git-core python-pip htop -y
+sudo apt-get install tmux zsh git-core python-pip htop wget tig -y
 sudo pip install jedi
 sudo pip install ipython
 git clone --recursive https://github.com/averrin/maximum-awesome.git
@@ -15,4 +15,5 @@ git clone git://github.com/kennethreitz/autoenv.git ~/.autoenv
 echo "alias averrin='tmux -u attach -t averrin || tmux -u new -s averrin'" >> ~/.bashrc
 echo "alias tester='tmux -u attach -t tester || tmux -u new -s tester'" >> ~/.bashrc
 cd
+wget https://raw.github.com/seebi/dircolors-solarized/master/dircolors.256dark
 echo "Done. type 'tmux -u' to start"
