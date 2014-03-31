9f10831a15110102d3ae5d3f659bc9e48db64bde
Averrin <averrin@gmail.com>
2014-03-31 13:52:47 +0400

    add vim

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-sudo apt-get install tmux zsh git-core python-pip htop wget tig -y
+sudo apt-get install tmux zsh git-core python-pip htop wget tig vim -y
 sudo pip install jedi
 sudo pip install ipython
 git clone --recursive https://github.com/averrin/maximum-awesome.git
