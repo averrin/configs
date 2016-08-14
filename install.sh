49a3b977af9cf47ac04e319877a602df7ef28df7
Averrin <averrin@gmail.com>
2016-08-14 13:56:28 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,5 @@
-sudo apt-get install tmux zsh git-core python-pip htop wget tig vim -y
-sudo pip install jedi
-sudo pip install ipython
-sudo pip install percol
-sudo pip install prompt-toolkit
-sudo pip install thefuck
+sudo apt-get install tmux zsh git-core python-pip htop wget vim -y
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone --recursive https://github.com/averrin/maximum-awesome.git
 cd ./maximum-awesome
 cp -r ./tmux.conf ~/.tmux.conf
@@ -14,11 +10,7 @@ cp -r ~/.vim/vim/* ~/.vim
 cp -r ./oh-my-zsh ~/.oh-my-zsh
 cd ~/.oh-my-zsh/plugins
 git clone git://github.com/zsh-users/zsh-syntax-highlighting.git
-git clone git://github.com/kennethreitz/autoenv.git ~/.autoenv
 git clone https://github.com/mhinz/vim-startify ~/.vim/bundle/vim-startify
-echo "alias averrin='tmux -u attach -t averrin || tmux -u new -s averrin'" >> ~/.bashrc
-echo "alias tester='tmux -u attach -t tester || tmux -u new -s tester'" >> ~/.bashrc
 cd
 wget https://raw.github.com/seebi/dircolors-solarized/master/dircolors.256dark
-curl http://beyondgrep.com/ack-2.14-single-file > ~/.local/bin/ack; chmod +x ~/.local/bin/ack
 echo "Done. type 'tmux -u' to start"
