a6b17eec30921fca663ce509cd8ceee3084a982a
Alexey Nabrodov <alexey.nabrodov@team.wrike.com>
2017-02-24 23:37:40 +0300

    add evil.el

---
+++ spacemacs.d/evil.el
@@ -0,0 +1,22 @@
+(defun bb/evil-delete (orig-fn beg end &optional type _ &rest args)
+  (let ((text (filter-buffer-substring beg end)))
+    (apply orig-fn beg end type _ args)
+    (evil-set-register ?z text)
+    (evil-set-register ?\" (evil-get-register ?0))
+    ))
+
+(defun averrin/evil-config()
+  (global-evil-mc-mode 1)
+
+  (vhl/define-extension 'evil 'evil-paste-after 'evil-paste-before
+                        'evil-paste-pop 'evil-move)
+  (vhl/install-extension 'evil)
+
+  (define-key evil-normal-state-map (kbd "C-SPC") 'evil-scroll-page-down)
+  (define-key evil-normal-state-map (kbd "C-S-SPC") 'evil-scroll-page-up)
+  (define-key evil-normal-state-map (kbd "gj") 'pop-global-mark)
+  (define-key evil-normal-state-map (kbd "q") 'kill-this-buffer)
+  (define-key evil-normal-state-map (kbd "gp") (kbd "\"zp"))
+
+  (advice-add 'evil-delete :around 'bb/evil-delete)
+  )
